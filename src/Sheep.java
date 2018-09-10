d7231c9761a41b9cc4477ea107bfa27d138217c8
Ziggy <sinare@ttu.ee>
2018-09-10 15:07:07 +0300

    :) DONE. Tests passed

--- src/Sheep.java
+++ src/Sheep.java
@@ -1,14 +1,40 @@
+import java.util.List;
+
 
 public class Sheep {
 
-   enum Animal {sheep, goat};
-   
-   public static void main (String[] param) {
-      // for debugging
+   enum Animal {
+      sheep, goat
+   };
+
+   public static void main(String[] param) {
+//        Animal[] animals = new Animal [4];
+//        animals[0] = Animal.goat;
+//        animals[1] = Animal.sheep;
+//        animals[2] = Animal.goat;
+//        animals[3] = Animal.sheep;
+//        reorder(animals);
    }
-   
-   public static void reorder (Animal[] animals) {
-      // TODO!!! Your program here
+
+   public static void reorder(Animal[] animals) {
+      int count = 0;
+
+      for (Animal animal : animals) {
+         if (animal == Animal.goat) {
+            count++;
+         }
+      }
+      // loop fills the arr with goats until count
+      for (int i = 0; i < count; i++) {
+         animals[i] = Animal.goat;
+      }
+
+      // loop fills remaining arr space with sheep
+      for (int i = count; i < animals.length; i++) {
+         animals[i] = Animal.sheep;
+      }
    }
 }
+
+
 
